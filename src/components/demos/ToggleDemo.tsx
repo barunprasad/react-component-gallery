c2101e0db057064a299a15ec7a6f3a1893fb952e
Barun Prasad <barun.prasad@hotmail.com>
2025-12-03 11:19:59 +0530

    add starter code

---
+++ src/components/demos/ToggleDemo.tsx
@@ -0,0 +1,10 @@
+// ToggleDemo.tsx (and similar for others)
+function ToggleDemo() {
+  return (
+    <div>
+      <p>Toggle Demo - To be implemented</p>
+    </div>
+  );
+}
+
+export default ToggleDemo;
